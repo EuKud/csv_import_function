de8995a9cfb0255f429e1ee65a55df5f3091f7e5
eugen <eugen.kudraschow@gmail.com>
2024-11-08 11:08:53 +0100

    configure http-server

--- testautomation/cypress/e2e/csv_import.cy.js
+++ testautomation/cypress/e2e/csv_import.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('CSV Import Function', () => {
+
+  beforeEach(() => {
+    cy.visit('/csv-import');
+  });
 })
